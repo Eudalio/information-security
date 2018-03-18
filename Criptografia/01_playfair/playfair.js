01cf37bba82887ae9fb28a741768582ea39be60c
eudalio <tonnydallios@gmail.com>
2018-03-18 17:29:17 -0300

    Método que cria a matriz com a chave - pronto

--- Criptografia/01_playfair/playfair.js
+++ Criptografia/01_playfair/playfair.js
@@ -2,27 +2,28 @@
 
 // Definindo o alfabeto a ser trabalhado
 let alfabeto = ['A','B','C','D','E',
-            'F','G','H','I','j',
+            'F','G','H','I','K',
             'L','M','N','O','P',
             'Q','R','S','T','U',
-            'V','W','X','Y','Z'
-            ];
+            'V','W','X','Y','Z'];
 
 let coluna = 0;
 let linha = 0;
-let chave = "carambola doida".toUpperCase();
+let chave = "peixe assado".toUpperCase();
 let mensagem = "O cara e bom mesmo";
 
 // Definindo a matriz
-let grid = [[],[],[],[],[]];
+let matriz = [[],[],[],[],[]];
 
-function tirarLetrasRepetidasChave(){
-    vetChave = chave.split('');
+function tirarLetrasRepetidasChave(vetChave){
     //console.log("vetChave: " + vetChave);
-    let aux = ' ';
+    let aux = '';
     //console.log("chave no inicio: " + chave);
 
     vetChave.forEach(element => {
+        if(element === 'J'){
+            element = 'I';
+        }
         if(aux.indexOf(element) === -1){
             aux += element;            
         }
@@ -31,4 +32,96 @@ function tirarLetrasRepetidasChave(){
     //console.log("chave no final: " + chave);
 }
 
-tirarLetrasRepetidasChave();
+tirarLetrasRepetidasChave(stringToArray(chave));
+
+function stringToArray(chave){
+    let arrayChave = [];
+    let aux = chave.split('');
+    for(let i = 0; i < aux.length; i++){
+        if(aux[i] !== ' '){
+            arrayChave.push(aux[i]);
+        }
+    }
+    return arrayChave;
+}
+
+function tirarLetraDuplicadasAlfabeto(palavraChave, alfabeto){
+    palavraChave.forEach( letra => {
+        let indice = alfabeto.indexOf(letra);
+        if(indice > -1){
+            alfabeto.splice(indice,1);
+        }
+    });
+    return alfabeto;
+}
+
+function preencherMatrizComChave(vetChave){
+    //1º Tentativa
+
+    /* const matrizChave = [...new Set(`${vetChave}${novoAlfabeto}`)]
+    console.log("VETCHAVE: " + vetChave);
+    console.log("NOVO ALFABETO: " + novoAlfabeto);
+    console.log("Matriz chave: " + matrizChave);
+
+    for(let i = 0; i < matrizChave.length; i += 5){
+        matriz.push(matrizChave.slice(i, (i+5)));
+    }
+
+    console.log('matriz final: ' + matriz); */
+    
+    //-------------------------------------------------
+
+    // 2º Tentativa    
+    
+    /* console.log("matriz antes: " + matriz);
+    vetChave.forEach(element => {     
+        if(coluna % 5 >= 0 || coluna === 0){
+            matriz[linha][coluna] = element;
+            coluna++;
+            //console.log("matriz meio if: " + matriz);
+        }
+        else{
+            linha++;
+            coluna = 0;
+        }
+    }); */
+
+    // ------------------------------------------------
+
+    // 3º Tentativa
+
+    /* for (const letra of vetChave) {
+        if((coluna % 5 >= 0 && coluna % 5 < 5) || coluna === 0){
+            matriz[linha][coluna] = letra;
+            coluna++;
+            console.log("matriz meio if: " + matriz);
+        }
+        else{
+            linha++;
+            coluna = 0;
+        }
+    }
+    */
+    
+    // -----------------------------------------------------------------
+
+    // 4º Tentativa
+
+    coluna = 0;
+    linha = 0;
+    for(let i = 0; i < chave.length; i++){
+        if( coluna % 5 !== 0 || coluna === 0 ){
+            matriz[linha][coluna] = chave[i];
+            coluna++;
+        }else{
+            linha ++;
+            coluna = 0;
+            i--;
+        }
+    }
+
+    // console.log("matriz depois: " + matriz);
+}
+
+preencherMatrizComChave(stringToArray(chave));
+
